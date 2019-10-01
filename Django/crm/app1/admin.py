b9e8fa55bc5150333a77caeff8cc5194af65998b
BinceAlocious <52814300+BinceAlocious@users.noreply.github.com>
2019-10-01 13:21:42 +0530

    Delete admin.py

--- Django/crm/app1/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from app1.models import Course,Batch,User,Register
-# Register your models here.
-admin.site.register(Course)
-admin.site.register(Batch)
-admin.site.register(User)
-admin.site.register(Register)
-
